770801fca6a26b397e78532ca9c24817f0b49b34
David Proto <davidproto@sbcglobal.net>
2024-12-23 23:22:06 -0500

    changed link styles in homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.html', // Matches all HTML files in src and subdirectories
-    './src/**/*.js',   // Matches all JS files in src and subdirectories
-    './src/**/*.jsx',  // Include JSX files if applicable
-    './src/**/*.ts',   // Include TypeScript files if applicable
-    './src/**/*.tsx'   // Include TSX files if applicable
+    './src/**/*.html', // For development files
+    './public/**/*.html', // For deployment files
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
   ],
   theme: {
     extend: {},
